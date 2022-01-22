67f8f805eef72ae664238ed68714f8eafac6cc4a
ganu453 <ganu453@gmail.com>
2022-01-22 15:14:25 +0530

    fixing code

--- Naiad/Runtime/Controlling/Controller.cs
+++ Naiad/Runtime/Controlling/Controller.cs
@@ -768,7 +768,8 @@ namespace Microsoft.Research.Naiad
 
             if (this.configuration.Endpoints != null)
             {
-                if(this.configuration.Endpoints.Length >= this.configuration.ProcessID){
+                if (this.configuration.ProcessID >= this.configuration.Endpoints.Length)
+                {
                     throw new ApplicationException("Intended process number (-p) should less then the number supplied to -n");
                 }
                 endpoint = this.configuration.Endpoints[this.configuration.ProcessID];
